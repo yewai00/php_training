c45e078eaf4a11917ec94514d1d022b551549bf6
Ye Wai <yewai.cs@gmail.com>
2021-12-15 10:41:33 +0630

    [master] finished php tutorial 1

---
+++ tutorials/tutorial_01/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tutorial_01</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="checkmake">
+	<?php	
+		$flag = 0;
+		for($row = 0; $row < 8; $row++) {
+			for($square = 0; $square < 8; $square++) {
+				if(!$flag) {
+					echo "<div class='white'></div>";
+					$flag = 1;
+				}
+				else {
+					echo "<div class='black'></div>";
+					$flag = 0;
+				}
+			}
+			echo "<br>";
+			if($flag) $flag = 0;
+			else $flag = 1;
+		}
+	?>
+	</div>
+</body>
+</html>
+
