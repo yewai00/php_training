93c0db5fc6f2ee85f8ffa486d45d98b22810b748
Ye Wai <yewai.cs@gmail.com>
2021-12-16 13:07:26 +0630

    [master] finished php tutorial 03

--- tutorials/tutorial_03/calculateAge.php
+++ tutorials/tutorial_03/calculateAge.php
@@ -6,7 +6,7 @@
         $sec_age = $now - $bday;
         $day_age = $sec_age / (60 * 60 * 24);
         $age = round($day_age / 365);
-        if($age){
+        if(floor($day_age / 365)){
             header("location: index.php?age=$age");
         }
         else {
