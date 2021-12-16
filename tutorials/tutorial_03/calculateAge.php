916a0c17dfeb42dc682f192750382bf893da5735
Ye Wai <yewai.cs@gmail.com>
2021-12-16 11:22:07 +0630

    [master] finished php tutorial 03

---
+++ tutorials/tutorial_03/calculateAge.php
@@ -0,0 +1,22 @@
+<?php 
+    $birthday = $_GET["birthday"];
+    $now = time();
+    $bday = strtotime($birthday);
+    if($now > $bday) {
+        $sec_age = $now - $bday;
+        $day_age = $sec_age / (60 * 60 * 24);
+        $age = round($day_age / 365);
+        if($age){
+            header("location: index.php?age=$age");
+        }
+        else {
+            $day_age = round($day_age);
+            header("location: index.php?day=$day_age");
+        }
+    }
+
+    elseif($now <= $bday) {
+        $age = 0;
+        header("location: index.php?age=$age");
+    }
+
---
+++ tutorials/tutorial_03/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top | tutorial 03</title>
+</head>
+<body>
+    <?php 
+        $month = date('m');
+        $day = date('d');
+        $year = date('Y');
+        $today = $year . '-' . $month . '-' . $day;
+    ?>
+    <h3>Today is <?php echo $today ?></h3>
+    <form action="calculateAge.php" method="GET">
+        <label for="date">Choose your birthday: </label>
+        <input type="date" name="birthday" id="date" value="<?php echo $today; ?>">
+        <input type="submit" value="calculate">
+    </form>
+    <?php 
+        if (isset($_GET['age'])) {
+            $age = $_GET['age'];
+            if($age) echo "<p> You are $age years old. </p>";
+            else echo "<p> You are not born yet!</p>";
+        }
+        
+        elseif (isset($_GET['day'])) {
+            $age = $_GET['day'];
+            if($age) echo "<p> You are $age days old baby.</p>";
+            else echo "<p>Congratulations! You are born today.</p>";
+        }
+    ?>
+</body>
+</html>
