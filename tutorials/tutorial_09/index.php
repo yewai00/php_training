517389434701d9818ae500a17af37333901c51b4
Ye Wai <yewai.cs@gmail.com>
2021-12-23 12:51:47 +0630

    [master] finished php tutorial 09

--- tutorials/tutorial_09/index.php
+++ tutorials/tutorial_09/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Top | tutorial 08</title>
+    <title>Top | tutorial 09</title>
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="container">
-        <h3>Tutorial 08</h3>
+        <h3>Tutorial 09</h3>
         <a href="create.php" class="home link">Create a person</a>
         <?php
             include_once('connect.php');
